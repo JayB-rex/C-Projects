2288aa46ffec41f885ab5e10d9ed7de647bfba8a
Jayden Bach <jaycomp88@gmail.com>
2026-02-13 14:03:46 +1100

    c++ files edited

--- matrix_multiplication/matrix_numbers.cpp
+++ matrix_multiplication/matrix_numbers.cpp
@@ -5,7 +5,28 @@
 #include "matrix_numbers.h"
 #include <iostream>
 
+
 int main() {
+    // declaring both matrix
+
+    int matrix_one[2][2] = {
+        {3, 2},
+        {2, 8}
+    };
+
+    int matrix_two[2][2] = {
+        {1, 2},
+        {3, 4}
+    };
+
+// first 3 * 1 + 2 * 3
+        // 3 * 2 + 2 * 4
+        //
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            std::cout << matrix_one[i][j] << " ";
+        }
+    }
 
 
 }
